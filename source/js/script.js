4771a750af67159765de9f89f8582941a06dfc0c
Artem Klimichev <aklimichevm@yandex.ru>
2022-12-25 14:19:51 +0300

    Исправляет ошибки

--- source/js/script.js
+++ source/js/script.js
@@ -1,34 +1,25 @@
 (function ($) {
-  // получаем доступ к элементу слайдера на странице
-  var $dragMe = $(".toggle-drag"),
-    // к слайдеру
+  var $dragMe = $(".slider-container__toggle-drag"),
     $container = $(".slider-container"),
-    // и картинке слева
-    $viewAfter = $(".view-after");
-  // используем свойство draggable из библиотеки с интерфейсом, чтобы получить координаты сдвига слайдера
+    $viewAfter = $(".slider-container__view-after");
   $dragMe.draggable({
     containment: "parent",
     drag: function () {
-      // при перемещении слайдера меняем ширину картинки слева в стилях
       $viewAfter.css({
         width: parseFloat($(this).css('left')) + 3
       });
     }
   });
-  // добавляем реакцию на клик по картинке
   $container.on("click", function (event) {
-    // получаем координаты клика, чтобы сместить туда слайдер
     var eventLeft = event.pageX - $container.offset().left - 15;
-    // плавно сдвигаем слайдер
     animateTo(eventLeft);
   });
-  // функция сдвига слайдера при клике, на входе получаем новые координаты границы картинок
+  animateTo("50%");
+
   function animateTo(_left) {
-    // анимируем сдвиг слайдера
     $dragMe.animate({
       left: _left
     }, 'slow', 'linear');
-    // и картинки
     $viewAfter.animate({
       width: _left
     }, 'slow', 'linear');
