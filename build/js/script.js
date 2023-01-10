5d303c47e81f8aa12b1cf6fe58a9592073c8afae
Temmatema <112764243+Temmatema@users.noreply.github.com>
2023-01-10 17:43:32 +0300

    Автоматизация

---
+++ build/js/script.js
@@ -0,0 +1,19 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+navMain.classList.add('main-nav--closed');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+    navToggle.classList.add('main-header__toggle-anim-in');
+    navToggle.classList.remove('main-header__toggle-anim-out');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+    navToggle.classList.add('main-header__toggle-anim-out');
+    navToggle.classList.remove('main-header__toggle-anim-in');
+  }
+});
